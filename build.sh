d6c2b494d528430abbcebdab801e60dd62ae430d
Juan Jimenez-Anca <cortopy@users.noreply.github.com>
2021-02-21 12:08:50 +0000

    upgrade 13 beta 11

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-GIT_BRANCH=version-12
+GIT_BRANCH=version-13-beta
 CONTAINER_REGISTRY=j3asolutions
-FRAPPE_VERSION=v12.15.0
-ERPNEXT_VERSION=v12.18.0
-SOCKETIO_VERSION=v12.14.0
+FRAPPE_VERSION=v13.0.0-beta.10
+ERPNEXT_VERSION=v13.0.0-beta.11
+SOCKETIO_VERSION=v13.0.0-beta.10
 
 FRAPPE_NGINX_TAG=$CONTAINER_REGISTRY/frappe-nginx:$GIT_BRANCH
 FRAPPE_WORKER_TAG=$CONTAINER_REGISTRY/frappe-worker:$GIT_BRANCH
@@ -17,51 +17,24 @@ ERPNEXT_WORKER_TAG=$CONTAINER_REGISTRY/erpnext-worker:$ERPNEXT_VERSION
 SOCKETIO_WORKER_TAG=$CONTAINER_REGISTRY/frappe-socketio:$SOCKETIO_VERSION
 
 #
-# NGINX
-#
-
-echo "Building image $FRAPPE_NGINX_TAG";
-
-docker build \
-  --pull \
-  --tag "$FRAPPE_NGINX_TAG" \
-  --build-arg GIT_BRANCH=$GIT_BRANCH \
-  --file build/frappe-nginx/Dockerfile \
-  .;
-
-docker push $FRAPPE_NGINX_TAG;
-
-echo "Building image $ERPNEXT_NGINX_TAG";
-
-docker build \
-  --pull \
-  --tag "$ERPNEXT_NGINX_TAG" \
-  --build-arg GIT_BRANCH=$GIT_BRANCH \
-  --build-arg DOCKER_REGISTRY_PREFIX=$CONTAINER_REGISTRY \
-  --file build/erpnext-nginx/Dockerfile \
-  .;
-
-docker push $ERPNEXT_NGINX_TAG;
-
-#
 # WORKER
 #
 
 echo "Building image $FRAPPE_WORKER_TAG";
 
-docker build \
+podman build \
   --pull \
   --tag "$FRAPPE_WORKER_TAG" \
   --build-arg GIT_BRANCH=$GIT_BRANCH \
   --file build/frappe-worker/Dockerfile \
   .;
 
-docker push $FRAPPE_WORKER_TAG;
+podman push $FRAPPE_WORKER_TAG;
 
 
 echo "Building image $ERPNEXT_WORKER_TAG";
 
-docker build \
+podman build \
   --pull \
   --tag "$ERPNEXT_WORKER_TAG" \
   --build-arg GIT_BRANCH=$GIT_BRANCH \
@@ -69,7 +42,34 @@ docker build \
   --file build/erpnext-worker/Dockerfile \
   .;
 
-docker push $ERPNEXT_WORKER_TAG;
+podman push $ERPNEXT_WORKER_TAG;
+
+#
+# NGINX
+#
+
+echo "Building image $FRAPPE_NGINX_TAG";
+
+podman build \
+  --pull \
+  --tag "$FRAPPE_NGINX_TAG" \
+  --build-arg GIT_BRANCH=$GIT_BRANCH \
+  --file build/frappe-nginx/Dockerfile \
+  .;
+
+podman push $FRAPPE_NGINX_TAG;
+
+echo "Building image $ERPNEXT_NGINX_TAG";
+
+podman build \
+  --pull \
+  --tag "$ERPNEXT_NGINX_TAG" \
+  --build-arg GIT_BRANCH=$GIT_BRANCH \
+  --build-arg DOCKER_REGISTRY_PREFIX=$CONTAINER_REGISTRY \
+  --file build/erpnext-nginx/Dockerfile \
+  .;
+
+podman push $ERPNEXT_NGINX_TAG;
 
 #
 # SCOKETIO
@@ -77,11 +77,11 @@ docker push $ERPNEXT_WORKER_TAG;
 
 echo "Building image $SOCKETIO_WORKER_TAG";
 
-docker build \
+podman build \
   --pull \
   --tag "$SOCKETIO_WORKER_TAG" \
   --build-arg GIT_BRANCH=$GIT_BRANCH \
   --file build/frappe-socketio/Dockerfile \
   .;
 
-docker push $SOCKETIO_WORKER_TAG;
+podman push $SOCKETIO_WORKER_TAG;
